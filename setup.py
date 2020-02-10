f6c0cdf480caea9d7e7795f521a539d9e28c5d7e
Lauren Adabie <lauren.adabie@gmail.com>
2020-02-10 08:19:26 -0600

    Addition of git url to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="usgs_waterservices_data",
       version='0.1',
       description="Wrapper to handle pulling data from USGS Water Services API",
-      url="",
+      url="https://github.com/ladabie/usgs_waterservices_data.git",
       author="Lauren Adabie",
       author_email="lauren.adabie@gmail.com",
       license='MIT',
